cc2a1bd3ea0a78721dc4d6054f2593b7d30c0747
Fran Ayrolo <fran.ayrolo@gmail.com>
2026-03-24 00:15:18 -0300

    agregué el primer arte y un escenario para probar

---
+++ PRUEBAS/base_minijuego.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+#para configurar el minijuego antes de arrancarlo
+#para arrancar el minijuego externamente
+#para consultar el progreso de un minijuego
+#para avisar si el minijuego termino y con que resultado bien/mal
+#para reiniciar el minijuego
--- PRUEBAS/character_body_2d.gd
+++ PRUEBAS/character_body_2d.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 100000.0
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
-	velocity = direction * SPEED
+	velocity = direction * SPEED * delta
 	#if velocity.x == 0.:
 	velocity.move_toward(Vector2.ZERO, SPEED)
+	%Sprite.flip_h = direction.x > 0
 	
 	move_and_slide()
